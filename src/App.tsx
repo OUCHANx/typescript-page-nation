e80336c6c8e57242926b3e85f2fc5d33e390fa01
ouchan <kingbook0628@gmail.com>
2026-05-18 22:42:13 +0900

    add react pagination project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import './App.css'
+
+export function App() {
+
+  useEffect(() => {
+    const getAlbums = async () => {
+      await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+      .then(res => res.json())
+      .then((album) => console.log(album));
+      };
+      getAlbums();
+    },[])
+  return <div className="App"></div>
+}
+export default App
