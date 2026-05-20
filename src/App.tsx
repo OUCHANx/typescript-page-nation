6d7ad386a14608baba6c8a559bf0b05c8b4aee1b
ouchan <kingbook0628@gmail.com>
2026-05-20 23:39:41 +0900

    状態変数

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 export function App() {
-
+  const [albums, setAlbums] = useState<string>([]);
   useEffect(() => {
     const getAlbums = async () => {
       await fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
